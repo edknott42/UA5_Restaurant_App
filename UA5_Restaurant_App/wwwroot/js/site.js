a7dd63ff5709d5a904c143806daeec82560d73de
Edward Knott <edknott04@gmail.com>
2024-06-28 17:22:59 +0100

    Menu, Takeaway

--- UA5_Restaurant_App/wwwroot/js/site.js
+++ UA5_Restaurant_App/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+$('#myTab a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
+
+
+function showDeliveryPopup() {
+    document.getElementById("myDeliveryCost").style.display = "flex";
+    document.body.style.overflow = "hidden";
+}
+
+function hideDeliveryPopup() {
+    document.getElementById("myDeliveryCost").style = "";
+    document.body.style = "";
+}
